d9ec7661f852dbce8c74fb0ec9192186799d47e2
= <pedroceolato2002@gmail.com>
2023-01-05 07:57:00 -0300

    refactor: theme validation change

--- js/theme.js
+++ js/theme.js
@@ -1,23 +1,28 @@
+let theme = 'dark'
+const toggle = document.getElementById('toggle')
+
 export function handleChangeTheme() {
     const storePrefs = localStorage.getItem('color-theme')
+    theme = storePrefs
 
-    if (storePrefs === 'light') {
+    if (theme === 'light') {
         document.body.classList.add('light')
         document.body.style.background = '#F3F7F3'
         
-    } else document.body.style.background = '#151515'
-
-    const toggle = document.getElementById('toggle')
+    } else {
+        document.body.style.background = '#151515'
+    }
 
     toggle.addEventListener("click", () => {
-        const iconMoon = document.querySelector('.ph-moon-bold')
-        const displayIconMoon = window.getComputedStyle(iconMoon, null).getPropertyValue('display')
 
-        if (displayIconMoon === 'block') {
+        theme = theme === 'light' ? 'dark' : 'light'
+
+        if (theme === 'dark') {
             document.body.classList.remove('light')
             document.body.style.background = '#151515'
 
             localStorage.setItem('color-theme', 'dark')
+
         } else {
             document.body.classList.add('light')
             document.body.style.background = '#F3F7F3'
@@ -26,4 +31,3 @@ export function handleChangeTheme() {
         }
     })
 }
-
